0ea72b70ec81e50925ea14e15bc30c8b31a343f6
Poonam <poonammshettar123@gmail.com>
2024-04-21 13:13:23 +0530

    Dp solution for catalan Numbers

---
+++ Class/catalan_dp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> dp(10, 0);
+    dp[0] = dp[1] = 1;
+    for (int i = 2; i < 10; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            dp[i] += dp[i - j - 1] * dp[j];
+        }
+    }
+    for (int i = 0; i < dp.size(); i++)
+    {
+        cout << dp[i] << '\t';
+    }
+}
